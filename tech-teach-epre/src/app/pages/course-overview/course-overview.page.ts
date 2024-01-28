f82a6f8eee96395514e1a8807b26edea11cd1c9c
Yazdan Musa <yazdanmusa@gmail.com>
2024-01-28 18:26:29 +0100

    fix icon and back btn

--- tech-teach-epre/src/app/pages/course-overview/course-overview.page.ts
+++ tech-teach-epre/src/app/pages/course-overview/course-overview.page.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { IonIcon, IonicModule } from "@ionic/angular";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { Course } from "src/model";
 import { DataService } from "src/app/services/data.service";
-import { HomePage } from "../home/home.page";
+import { addIcons } from "ionicons";
+import { chevronBackOutline } from "ionicons/icons";
 
 @Component({
   selector: "app-course-overview",
@@ -19,13 +20,22 @@ export class CourseOverviewPage implements OnInit {
   course: Course | undefined;
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private dataService: DataService,
-  ) {}
+  ) {
+    addIcons({
+      chevronBackOutline,
+    });
+  }
 
   ngOnInit() {
     this.courseId = this.route.snapshot.paramMap.get("id");
     if (this.courseId) {
       this.course = this.dataService.getCourseById(this.courseId);
     }
+  }
+
+  goBack() {
+    this.router.navigate(["/"]);
   }
 }
